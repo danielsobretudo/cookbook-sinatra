2b6bb5bd81f9aea0ba957f60a44d52f0dd5727c5
Daniel Castro <danielsobretudo@yahoo.com.br>
2020-09-13 06:56:34 -0300

    working with sinatra

--- app.rb
+++ app.rb
@@ -7,6 +7,25 @@ configure :development do
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
-get '/' do
-  'Hello world!'
+# get '/' do
+#   erb :index
+# end
+
+get '/about' do
+  erb :about
 end
+
+get '/team/:username' do
+  puts params[:username]
+  "The username is #{params[:username]}"
+end
+
+get '/' do
+  @usernames = ['ssaunier', 'Papillard']
+  erb :index
+end
+
+# get '/team/:username' do
+#   binding.pry  # <= code will stop here for HTTP request localhost:4567/team/someone
+#   # [...]
+# end
